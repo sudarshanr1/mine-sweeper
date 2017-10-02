820ab80529d6c067e67fc19a679367282a33fe23
Sudarshan Ravindranath <sudarshanravindranath@Sudarshans-MacBook-Pro.local>
2017-10-01 18:44:40 -0700

    heroku changes

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
+app.use(function(request, response, next){
+  if(request.headers['x-forwarded-proto'] === 'https') {
+    response.redirect('http://' + request.hostname + request.url);
+  } else {
+    next();
+  }
+});
 app.use(express.static('public'));
 app.listen(PORT,function(){
   console.log('Express server '+PORT+' is up');
